c8a462ce4c0990d8a76ef02352762b557a1eaf84
Yauheniya Viarbitskaya <evgeniya.verbiczkaya98@gmail.com>
2018-09-28 22:26:32 +0300

    zer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let counter = Math.trunc(number / 5);
+    let zeros = counter;
+    while (counter > 1) {
+        counter = Math.trunc(counter / 5);
+        zeros = zeros + counter;
+    }
+    return zeros;
 }
